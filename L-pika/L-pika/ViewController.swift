d1aa75011e08b7e964584141f719021003210e5f
史 翔新 <shi_xiangxin@mages.co.jp>
2016-11-18 13:23:37 +0900

    Initial project and license

---
+++ L-pika/L-pika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L-pika
+//
+//  Created by 史　翔新 on 2016/11/18.
+//  Copyright © 2016年 net.crazism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
